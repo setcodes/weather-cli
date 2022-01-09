5ead29833f090a9acca6deed1ae5b3bed4f05d71
Egor Maximov <maximov.devs@gmail.com>
2022-01-09 14:46:17 +0300

    add storage service

---
+++ services/storage.service.js
@@ -0,0 +1,36 @@
+import { homedir } from "os";
+import { join } from "path";
+import { promises } from "fs";
+
+const filePath = join(homedir(), "weather-data.json");
+
+const setKeyValue = async (key, value) => {
+    let data = {};
+    if (await isExist(filePath)) {
+        const file = await promises.readFile(filePath);
+        data = JSON.parse(file);
+    }
+    data[key] = value;
+    await promises.writeFile(filePath, JSON.stringify(data));
+}
+
+const getKeyValue = async () => {
+    if (await isExist(filePath)) {
+        const file = await promises.readFile(filePath);
+        let data = JSON.parse(file);
+        return data[key];
+    }else {
+        return undefined;
+    }
+}
+
+const isExist = async (path) => {
+   try {
+       await promises.stat(path);
+       return true;
+   }catch (e) {
+       return false;
+   }
+}
+
+export {setKeyValue};
--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env_node
 import {getArgs} from './helpers/args.js';
 import {printError, printSuccess, printHelp} from "./services/log.service.js";
+import {setKeyValue} from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+    try {
+        await  setKeyValue("token", token);
+        printSuccess("Токен сохранен!");
+    }catch (e) {
+        printError(e.message);
+    }
+}
 
 const initCli = () => {
     const args = getArgs(process.argv);
@@ -11,7 +21,7 @@ const initCli = () => {
         //set sity
     }
     if (args.t) {
-        // set token
+        return saveToken( args.t);
     }
 }
 initCli();
